111fa0e715aa8b7ad47bc9a215571c6a157a0f40
Abdulmuin1 <abusuleimanibnukhelil@gmail.com>
2023-02-26 07:33:25 +0300

    initial commit

--- Services/HousesService.cs
+++ Services/HousesService.cs
@@ -28,11 +28,11 @@ public class HousesService
     public async Task<House?> GetAsync(string id) =>
         await _housesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
-    public async Task CreateAsync(House newBook) =>
-        await _housesCollection.InsertOneAsync(newBook);
+    public async Task CreateAsync(House newHouse) =>
+        await _housesCollection.InsertOneAsync(newHouse);
 
-    public async Task UpdateAsync(string id, House updatedBook) =>
-        await _housesCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+    public async Task UpdateAsync(string id, House updatedHouse) =>
+        await _housesCollection.ReplaceOneAsync(x => x.Id == id, updatedHouse);
 
     public async Task RemoveAsync(string id) =>
         await _housesCollection.DeleteOneAsync(x => x.Id == id);
